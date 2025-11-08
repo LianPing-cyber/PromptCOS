4713c3c4ea0b3035eee8162d83334ec4ae46aaf9
Lian-Ping <wsmgga@gmail.com>
2025-11-08 23:40:46 +0800

    delete old files

--- paths.py
+++
@@ -1,9 +0,0 @@
-codegemma2b = ""
-codegemma7b = ""
-gemma_2b = ""
-gemma2_2b_it = ".../gemma2-2b-it"
-tinyllama = ".../TinyLlama-1.1B-Chat-v1.0"
-deepseek_d_qwen = ".../models/deepseek-r1-distilled-qwen-1.5b"
-bert = ".../models/bert"
-
-manual = ".../manual"
